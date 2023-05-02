0691e18f22882141255631abbd51bc65037d90ee
srinivas-kandlikar <srinivas.us231@gmail.com>
2023-05-02 12:08:09 -0300

    backpack.js

---
+++ backpack.js
@@ -0,0 +1,19 @@
+class Backpack{
+constructor(
+name,
+volume,
+color,
+pocketNum,
+strapLengthL,
+strapLengthR
+)
+{
+this.name = name;
+this.volume = volume;
+this.color = color;
+this.pocketNum = pocketNum;
+this.strapLengthL = strapLengthL;
+this. strapLengthR= strapLengthR;
+}
+}
+export default Backpack;
