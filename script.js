51a03bdf6af4aff1bd4ab136ba4fe50db11cafac
srinivas kandlikar <srinivas.us231@gmail.com>
2023-05-02 11:56:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+import Backpack from "./backpack.js";
+
+const everydayPack = new Backpack(
+"Everyday Backpack",
+30,
+"grey",
+15,
+20,
+26
+);
+
+console.log("The everydayPack object:", everydayPack);
+console.log("The pocketNum value:", everydayPack.PocketNum);
